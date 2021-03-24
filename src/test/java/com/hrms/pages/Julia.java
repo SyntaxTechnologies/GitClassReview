8fdef59634d4426e122424cfaf88596a478060d5
iamdez99 <diegojuarezbusiness@gmail.com>
2021-03-23 21:42:58 -0400

    ..

--- src/test/java/com/hrms/pages/Julia.java
+++ src/test/java/com/hrms/pages/Julia.java
@@ -7,5 +7,6 @@ public class Julia {
 	
 	public void name() {
 		System.out.println("My name is Julia");
+		System.out.println("I will get a job and take Diego Sumair and Asel to Fogo de chao");
 	}
 }
