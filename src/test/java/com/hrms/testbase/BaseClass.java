ac8bfe17a6380d1179dcb602664efa54c9bdfdcc
iamdez99 <diegojuarezbusiness@gmail.com>
2021-03-24 20:13:24 -0400

    FRAMEWORK - Added headless execution properties

--- src/test/java/com/hrms/testbase/BaseClass.java
+++ src/test/java/com/hrms/testbase/BaseClass.java
@@ -1,48 +1,63 @@
 package com.hrms.testbase;
 
-import com.hrms.utils.ConfigsReader;
-import com.hrms.utils.Constants;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.concurrent.TimeUnit;
+import com.hrms.utils.ConfigsReader;
+import com.hrms.utils.Constants;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
-    public static WebDriver driver;
+	public static WebDriver driver;
 
-    /**
-     * this method will open a browser, set up configuration and navigate to the URL
-     */
-    public static void setUp() {
+	public static void setUp() {
 
-        ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
-        switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new RuntimeException("Ivalid browser");
-        }
-        driver.get(ConfigsReader.getPropertyValue("url"));
-        //driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
-        PageInitializer.initializePageObjects();
-    }
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		//Look at this line
+		String headless = ConfigsReader.getPropertyValue("headless");
+		
 
-    /**
-     * this method will close any open browser
-     */
-    public static void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
+
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			if(headless.equalsIgnoreCase("true")) {
+				chromeOptions.setHeadless(true);
+				driver = new ChromeDriver(chromeOptions);
+			}else {
+				driver = new ChromeDriver();
+			}
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		default:
+			throw new RuntimeException("Browser is not supported");
+		}
+
+		//driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.get(ConfigsReader.getPropertyValue("applicationUrl"));
+		PageInitializer.initializePageObjects();
+	}
+
+	
+	public static void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
